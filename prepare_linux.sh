3c7fd577278641c6b8dfd6a191dbb8ee1fcd2cee
Arash Delijani <arashdelijani@gmail.com>
2025-08-21 11:45:07 -0400

    uv commands

--- prepare_linux.sh
+++ prepare_linux.sh
@@ -1,2 +1,7 @@
 sudo apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 sudo apt-get install build-essential swig  git
+
+pip install uv
+uv pip compile requirements.in -o requirements.txt
+uv pip install -r requirements.txt
+
