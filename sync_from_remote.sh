8931f3145d876de5c186d32ccfbb04af9fabcdf7
Arash Delijani <arashdelijani@gmail.com>
2025-08-26 17:22:15 -0400

    update to hw3

--- sync_from_remote.sh
+++ sync_from_remote.sh
@@ -1,3 +1,3 @@
 rsync -avn \
-  --exclude='hw2/hw2-venv' --exclude='hw3/hw3-venv' --include='*/' --include='*.py' --exclude='*' \
-  root@vast-nj:/root/homework_fall2023/homework_fall2023/ ~/workspace/cs285/homework_fall2023/
+  --exclude='hw3/hw3-venv' --include='*/' --include='*.py' --exclude='*' \
+  root@vast-nj:/root/homework_fall2023 ~/workspace/cs285/
