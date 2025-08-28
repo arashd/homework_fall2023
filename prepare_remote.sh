53a530dc14557f13881a8a7fc568b4f0295641a4
Arash Delijani <arashdelijani@gmail.com>
2025-08-28 15:25:03 -0400

    fix prepare_remote.sh

--- prepare_remote.sh
+++ prepare_remote.sh
@@ -1,13 +1,23 @@
+# take two arguments: the hw number (1, 2, 3) and the remote name, then do the pip compile and install there.
+
+# check if the arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <hw_number> <remote_name>"
+    exit 1
+fi
+
+# install uv
+pip install uv
+
+# install dependencies
 sudo apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 sudo apt-get install build-essential swig  git
 
 # for each of the hw directories:
-pip install uv
-
-cd "hw3"
-yes n | uv venv "hw3-venv"
-source "hw3-venv/bin/activate"
+cd "hw$1"
+rm -rf "hw$1-venv"
+uv venv "hw$1-venv"
+source "hw$1-venv/bin/activate"
 uv pip compile requirements.in -o requirements.txt
 uv pip install -r requirements.txt
-cd ..
-
+source "hw$1-venv/bin/activate"
